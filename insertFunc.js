b386e8e62b3ec16979d01c01d8f8cc1806addeeb
Hai Tang <402696415@qq.com>
2016-02-18 23:32:49 -0500

    Feb 18th

--- insertFunc.js
+++ insertFunc.js
@@ -3,4 +3,8 @@ function insertParagraph(text){
 	str += text;
 	str += "</p>";
 	document.write(str);
+	window.onload = function(){
+		var testdiv = document.getElementById("testdiv");
+		alert(testdiv.innerHTML);
+	}
 }
