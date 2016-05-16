bd48cc9efae2475fab7b87dfe117d783c2937033
Hai Tang <402696415@qq.com>
2016-05-16 09:54:39 +0800

    May 16th

--- ch7/scripts/example.js
+++ ch7/scripts/example.js
@@ -4,3 +4,12 @@ function insertParagraph(text){
 	str += "</p>";
 	document.write(str);
 }
+
+window.onload = function(){
+	var para = document.createElement("p");
+	var testdiv = document.getElementById("testdiv");
+	testdiv.appendChild(para);
+
+	var txt = document.createTextNode("Hello World!");
+	para.appendChild(txt);
+}
